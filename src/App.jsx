ace71fd9b209a4780d2f3204fa6233f3463e8c50
uselessleaff <jianthelong@gmail.com>
2025-01-26 17:20:49 -0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Home'
 import Footer from './Components/Footer/Footer'
 
+import { Route, Routes } from "react-router-dom";
+
+
+
 const App = () => {
   return (
     <div>
         <Navbar />
+
         <Home />
-        <Footer/>
+        <Footer />
     </div>
   )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Footer.css'
 import footer_logo from '../../assets/footer_logo.svg'
 import user_icon from '../../assets/user_icon.svg'
+import { FaCross } from "react-icons/fa";
 import ul from '../../assets/ul.png'
 
 
@@ -10,27 +11,31 @@ const Footer = () => {
     <div className='footer'>
         <div className="footer-top">
             <div className="footer-top-left">
-                <img src={ul} alt="" />
-                <p>sheesh</p>
+                {/* <img src={ul} alt="" /> */}
+                <div>
+                    <FaCross /> <p>soli deo gloria</p>
+                </div>
             </div>
-            {/* <div className="footer-top-right">
-                <div className="footer-email-input">
+            <div className="footer-top-right">
+                <p>But he said to me, <span>“My grace is sufficient for you, for my power is made perfect in weakness.”</span> Therefore I will boast all the more gladly about my weaknesses, so that Christ’s power may rest on me. <br />
+                    2 Corinthians 12:9</p>
+                {/* <div className="footer-email-input">
                     <img src={user_icon} alt="" />
                     <input type="email" placeholder='enter your email' />
                 </div>
                 <div className="footer-subscribe">
                     subscribe
-                </div>
-            </div> */}
+                </div> */}
+            </div>
         </div>
         <hr />
         <div className="footer-bottom">
             <p className="footer-bottom-left">
-                copyright by me or smth
+                some text
             </p>
             <div className="footer-bottom-right">
-                <p>terms of service?</p>
-                <p>connect with me</p>
+                <p>more text</p>
+                <p>bottom right corner</p>
             </div>
         </div>
     </div>
